cc2d4f02fabcefa7ed723ca0a2ebb5f7221fc032
2NTH <hieuntctk42@gmail.com>
2020-08-01 16:50:30 +0700

    01.Inject JS code into JSX

--- 01.basic/src/App.js
+++ 01.basic/src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       <h1>Hello world</h1>
       <Person />
+      <Person />
+      <Person />
     </div>
   );
 }
--- 01.basic/src/Person/Person.js
+++ 01.basic/src/Person/Person.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Person = () => {
-  return <div>I am a person!</div>;
+  return (
+    <div>
+      I am a person! and I am {Math.floor(Math.random() * 30)} years old
+    </div>
+  );
 };
 
 export default Person;
