4d76ec4e41234a9cd3faef02845120676470d43f
2NTH <hieuntctk42@gmail.com>
2020-08-01 17:23:07 +0700

    01.Rerender when state changes

--- 01.basic/src/App.js
+++ 01.basic/src/App.js
@@ -2,17 +2,47 @@ import React from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-      <Person name="Max" age="28" />
-      <Person name="Manu" age="29">
-        My hobbies: racing
-      </Person>
-      <Person name="Stephenie" age="25" />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephenie', age: 26 }
+    ]
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephenie', age: 76 }
+      ]
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello world</h1>
+        <button onClick={() => this.switchNameHandler()}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My hobbies: racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
